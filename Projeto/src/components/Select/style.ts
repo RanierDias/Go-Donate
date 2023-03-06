5f7160b81aacd551bdc12c533ddcba66bc043aa9
Ranier Dias <ranierfernandodias@gmail.com>
2023-03-06 17:29:42 -0300

    Project Base

--- Projeto/src/components/Select/style.ts
+++ Projeto/src/components/Select/style.ts
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
 const Div = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  position: relative;
   background-color: var(--gray-80);
 `
 
