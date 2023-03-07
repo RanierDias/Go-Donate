97942109d6d04708dcd87f3ffc04381859ec3486
Fabio Luiz <fabioeeereed@gmail.com>
2023-03-07 10:49:52 -0300

    adiciona arquivo API.ts na services, e fontfamily no reset

--- Projeto/src/styles/reset.ts
+++ Projeto/src/styles/reset.ts
@@ -1,10 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 const Reset = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
   * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+    font-family: 'Inter', sans-serif;
   }
 
   a {
