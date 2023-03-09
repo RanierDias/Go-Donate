91a4886f127b195a5705f2f53da1d889c8b600ea
Ranier Dias <ranierfernandodias@gmail.com>
2023-03-09 11:34:38 -0300

    feat: add form to modal

---
+++ Projeto/src/components/ModalForm/Fundraising/index.tsx
@@ -0,0 +1,43 @@
+import Form from "../style";
+
+const FormFundraising = () => {
+  return (
+    <Form>
+      <div>
+        <label htmlFor="teste1">Nome</label>
+        <input type="text" placeholder="Digíte o nome da campanha" />
+      </div>
+      <div>
+        <label htmlFor="teste2">Data de início</label>
+        <input type="date" />
+      </div>
+      <div>
+        <label htmlFor="teste3">Data de fechamento</label>
+        <input type="date" />
+      </div>
+      <div>
+        <label htmlFor="teste">Número de Contato</label>
+        <input type="tel" placeholder="Digíte o telefone principal" />
+      </div>
+      <div>
+        <label htmlFor="teste4">Cidade</label>
+        <input type="text" placeholder="Digíte o nome da cidade" />
+      </div>
+      <div>
+        <label htmlFor="teste5">Estado</label>
+        <input type="text" placeholder="Digíte o nome do estado" />
+      </div>
+      <div>
+        <label htmlFor="teste6">Endereço</label>
+        <input type="text" placeholder="Digíte a rua e número" />
+      </div>
+
+      <div>
+        <label htmlFor="teste7">Descrição</label>
+        <textarea placeholder="Descreva mais o evento aqui"></textarea>
+      </div>
+    </Form>
+  );
+};
+
+export default FormFundraising;
